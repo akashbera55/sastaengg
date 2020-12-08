37777ba0164424eee3e46444e9f3751e09b162ba
sumit <sumit@sumit-PC>
2020-12-08 11:29:21 +0530

    New Test Case Added

--- src/basepack/BaseTest.java
+++ src/basepack/BaseTest.java
@@ -3,7 +3,6 @@ package basepack;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
---
+++ src/testf/TestFramework2.java
@@ -0,0 +1,13 @@
+package testf;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestFramework2 
+{
+	@Test
+public void rahul()
+{
+		Reporter.log("rohit",true);
+}
+}
